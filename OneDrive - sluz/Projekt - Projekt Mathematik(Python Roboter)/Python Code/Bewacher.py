9e49345cf1220b633bd24b285c1c59d9fe2499f0
Vorname Nachname <deine@email.ch>
2025-11-25 15:20:50 +0100

    Projektanforderung & Abgabeziele

---
+++ OneDrive - sluz/Projekt - Projekt Mathematik(Python Roboter)/Python Code/Bewacher.py
@@ -0,0 +1,16 @@
+#Projektanforderung: 
+#Wächter-Roboter (Security Bot): Entwickeln Sie einen Roboter, der wie ein Wachposten funktioniert. 
+# Aufgabe: Der Roboter soll einen Bereich überwachen und einen Alarm auslösen, wenn jemand zu nahe kommt. 
+# Nutzen Sie den Ultraschall- oder IR-Sensor, um Bewegungen/Annäherungen zu erkennen. 
+# Zum Beispiel kann der Roboter patrouillieren (hin- und herfahren) und bei einem erkannten Eindringling anhalten, 
+# laut Geräusche machen und auf seinem Display eine Warnung anzeigen. Bonus-Idee: Mit dem Gyrosensor könnte der Roboter 
+# immer wieder seine Ausgangsrichtung einnehmen (wie ein Wächter, der sich nach einer Drehung wieder nach vorn ausrichtet).
+
+#Abgabe des Projekts:
+#Bitte gebt bei der Abgabe eures EV3 Pybricks–MicroPython–Codes Folgendes mit ab:
+#Programmdatei: eure .py-Datei mit allen Funktionen und der Hauptroutine
+#Kurzbeschreibung im Code: kurze Erklärung, was das Programm macht und wie es aufgebaut ist
+#Hinweis auf KI-Einsatz: im Kommentar markieren, ob und wie KI (oder ähnliche Tools) verwendet wurden
+#Installationshinweise im Code: verwendete EV3-Ports und benötigte Bibliotheken dokumentieren
+#Teilnehmer am Projekt: Namen in Zeile 2 (nach <code>#!/usr/bin/env pybricks-micropython</code>)
+
